5a7c46dc9849426034e821453d51241b37b7ad45
Steve McDougall <juststevemcd@gmail.com>
2022-04-21 10:47:21 +0100

    Parsing openapi to get paths

--- src/Parser.php
+++ src/Parser.php
@@ -4,14 +4,56 @@ declare(strict_types=1);
 
 namespace JustSteveKing\YamlParser;
 
+use JustSteveKing\YamlParser\Exceptions\ParserException;
+use Symfony\Component\Yaml\Yaml;
+
 final class Parser
 {
     public function __construct(
         private readonly string $file,
+        private array $contents = [],
     ) {}
+
+    /**
+     * @return mixed
+     */
+    public function raw(): mixed
+    {
+        return Yaml::parseFile(
+            filename: $this->file,
+        );
+    }
+
+    public function boot(): void
+    {
+        $this->contents = $this->raw();
+    }
+
+    public function paths(): array
+    {
+        return $this->get(
+            key: 'paths',
+        );
+    }
+
+    public function get(string $key): mixed
+    {
+        if (! isset($this->contents[$key])) {
+            throw new ParserException(
+                message: "Failed to find key [$key] in file [$this->file]",
+            );
+        }
+
+        return $this->contents[$key];
+    }
 
     public function file(): string
     {
         return $this->file;
+    }
+
+    public function contents(): array
+    {
+        return $this->contents;
     }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use JustSteveKing\YamlParser\Exceptions\ParserException;
 use JustSteveKing\YamlParser\Parser;
 
 beforeEach(fn () =>
@@ -14,4 +15,58 @@ it('can create a new parser', function () {
     expect(
         $this->parser,
     )->toBeInstanceOf(Parser::class);
+});
+
+it('can get the file being passed to the parser', function () {
+    expect(
+        $this->parser->file(),
+    )->toEqual(__DIR__ . '/Fixtures/openapi.yaml');
+});
+
+it('can parse the file passed in', function () {
+    expect(
+        $this->parser->raw()
+    )->toBeArray()->toContain(
+        'trello.com'
+    );
+});
+
+it('can boot the parser', function () {
+    expect(
+        $this->parser->contents()
+    )->toBeArray()->toEqual([]);
+
+    $this->parser->boot();
+
+    expect(
+        $this->parser->contents()
+    )->toBeArray()->toContain(
+        'trello.com'
+    );
+});
+
+it('can get an item from the parser', function () {
+    $this->parser->boot();
+
+    expect(
+        $this->parser->get(
+            key: 'host',
+        )
+    )->toBeString()->toEqual('trello.com');
+});
+
+it('throws an exception if the key can\'t be found in the open api file', function (string $string) {
+    $this->parser->boot();
+
+    $this->parser->get(
+        key: $string,
+    );
+})->throws(ParserException::class)->with('strings');
+
+it('can get the api paths', function () {
+    $this->parser->boot();
+
+    expect(
+        $this->parser->paths(),
+    )->toBeArray();
 });
